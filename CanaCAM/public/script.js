7a3872b9afa59ab753267b58bc2b8ca02f7531b2
Brandon-Faulkner <bxf8125@mavs.uta.edu>
2023-05-13 13:01:27 -0500

    Made website PWA worthy with icons and installability and offline page.

--- CanaCAM/public/script.js
+++ CanaCAM/public/script.js
@@ -1,3 +1,18 @@
+// Ensure that the browser supports the service worker API
+if (navigator.serviceWorker) {
+  // Start registration process on every page load
+  window.addEventListener('load', () => {
+      navigator.serviceWorker
+          // The register function takes as argument
+          // the file path to the worker's file
+          .register('service-worker.js')
+          // Gives us registration object
+          .then(reg => console.log('Service Worker Registered'))
+          .catch(swErr => console.log(
+                `Service Worker Installation Error: ${swErr}}`));
+    });
+}
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import { getDatabase, ref, onValue, child, push, set } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
@@ -16,7 +31,6 @@ const firebaseConfig = {
 // Initialize Firebase and Database
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-
 
 //#region EVENT CALENDER
 
