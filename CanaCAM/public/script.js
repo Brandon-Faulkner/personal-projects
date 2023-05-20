734cc79243ad7e2f803a3f0fdc8b674ea5ab3cac
Brandon-Faulkner <bxf8125@mavs.uta.edu>
2023-05-19 20:46:02 -0500

    Got planning tab up and running. Now need to fix event listener functions and work on dropdown menus.

--- CanaCAM/public/script.js
+++ CanaCAM/public/script.js
@@ -35,7 +35,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const groupRef = ref_db(database, "Groups/");
-const planRef = ref_db(database, "Personal/");
+const planRef = ref_db(database, "GroupsInfo/");
 const userRef = ref_db(database, "Users/");
 const storage = getStorage(app);
 const auth = getAuth(app);
@@ -64,7 +64,11 @@ const signupButon = document.getElementById('signup-button');
 
 const mainScreen = document.getElementById('main-page');
 const tabPlanning = document.getElementById('tab-planning');
+const planningBlocked = document.getElementById('planning-blocked');
 const planningIntro = document.getElementById('tab-planning-intro');
+const planningImg = document.getElementById('main-image');
+const contactBtn = document.getElementById('contact-button');
+const planningWeekSelectParent = document.getElementById('plan-week-select-grid');
 const hostSelection = document.getElementById('host-selection');
 //const planWeekSelection = document.getElementById('plan-week-selection');
 const planCurrWeek = document.getElementById('plan-current-week');
@@ -78,14 +82,17 @@ const profileSection = document.getElementById('profile-section');
 
 //Arrays to hold the data of each week
 var currWeekArr = [], nextWeekArr = [], futureWeekArr = [];
+//Arrays to hold group information
+var groupInfoArr = [];
 
 //#region Authentication Functions
 // When the Auth state changes
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    // Signed in
+    // Signed in Anonymously
     if (user?.isAnonymous) {
-      OverviewSetup(groupRef);
+      OverviewSetup(groupRef, true);
+
       tabPlanning.addEventListener('click', function () {
         loginScreen.classList.add('show');
         mainScreen.classList.add('disable-click');
@@ -93,7 +100,12 @@ onAuthStateChanged(auth, (user) => {
       loginCloseBtn.addEventListener('click', function () {
         loginScreen.classList.remove('show');
         mainScreen.classList.remove('disable-click');
-      })
+      });
+    }
+    //Signed in with Account
+    else {
+      OverviewSetup(groupRef, false);
+
     }
 
   } else {
@@ -106,12 +118,10 @@ onAuthStateChanged(auth, (user) => {
 function AnonymousSignIn(auth) {
   signInAnonymously(auth)
     .then(() => {
-      console.log("Signed in");
+      console.log("Signed in Anonymously");
     })
     .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode + ": " + errorMessage);
+      console.log(error.code + ": " + error.message);
     });
 }
 
@@ -135,9 +145,7 @@ function SignInEmailAndPassword(auth, email, password) {
       console.log("Signed in with Email: " + user);
     })
     .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorCode + ": " + errorMessage);
+      console.log(error.code + ": " + error.message);
       return false;
     });
 }
@@ -185,7 +193,13 @@ signupButon.addEventListener('click', function (e) {
   var isEmailValid = validateEmail(email);
   var isPasswordValid = validatePassword(password, confirmPass);
 
-
+  //Upgrade annonymous account if data is valid
+  if (isEmailValid && isPasswordValid) {
+    const credential = EmailAuthProvider.credential(email, password);
+    UpgradeAnonymous(auth, credential);
+  } else {
+    console.log("Sign up data is invalid");
+  }
 
 });
 
@@ -216,7 +230,7 @@ signupImg.addEventListener('change', function () {
 //#endregion Login Functions
 
 //#region Overview Setup
-function OverviewSetup(groupRef) {
+function OverviewSetup(groupRef, isAnonymous) {
   onValue(groupRef, (snapshot) => {
     //Clear arrays to not have duplicate data
     currWeekArr = []; nextWeekArr = []; futureWeekArr = [];
@@ -234,15 +248,15 @@ function OverviewSetup(groupRef) {
 
       //Get the Day and times for each week
       childSnapshot.child("Weeks").child("Current Week").forEach((day) => {
-        const currWeekDay = { group: groupKey, day: day.key, time: day.val(), host: hostName, location: genLocation, address: null, email: null, phone: null };
+        const currWeekDay = { group: groupKey, day: day.key, time: day.val(), host: hostName, location: genLocation };
         currWeekArr.unshift(currWeekDay);
       });
       childSnapshot.child("Weeks").child("Next Week").forEach((day) => {
-        const nextWeekDay = { group: groupKey, day: day.key, time: day.val(), host: hostName, location: genLocation, address: null, email: null, phone: null };
+        const nextWeekDay = { group: groupKey, day: day.key, time: day.val(), host: hostName, location: genLocation };
         nextWeekArr.unshift(nextWeekDay);
       });
       childSnapshot.child("Weeks").child("Future Week").forEach((day) => {
-        const futureWeekDay = { group: groupKey, day: day.key, time: day.val(), host: hostName, location: genLocation, address: null, email: null, phone: null};
+        const futureWeekDay = { group: groupKey, day: day.key, time: day.val(), host: hostName, location: genLocation };
         futureWeekArr.unshift(futureWeekDay);
       });
 
@@ -275,6 +289,9 @@ function OverviewSetup(groupRef) {
         CreateOverviewTableRow(futureWeekArr, elem, overviewFutureWeek);
       });
     });
+
+    //Now setup planning tab
+    PlanningSetup(planRef, 'Group 1', isAnonymous);
 
     //Remove loading indicator
     if (!loader.classList.contains('hide')) {
@@ -314,9 +331,9 @@ function CreateOverviewTableHeader(day, parentElem) {
 
   var tableHeader = document.createElement('li');
   tableHeader.classList.add('table-header');
-  var headerDiv1 = document.createElement('div'); headerDiv1.className = "col col-1"; headerDiv1.textContent = "Time"; tableHeader.appendChild(headerDiv1);
-  var headerDiv2 = document.createElement('div'); headerDiv2.className = "col col-2"; headerDiv2.textContent = "General Location"; tableHeader.appendChild(headerDiv2);
-  var headerDiv3 = document.createElement('div'); headerDiv3.className = "col col-3"; headerDiv3.textContent = "Host"; tableHeader.appendChild(headerDiv3);
+  var headerDiv1 = document.createElement('div'); headerDiv1.className = "col"; headerDiv1.textContent = "Time"; tableHeader.appendChild(headerDiv1);
+  var headerDiv2 = document.createElement('div'); headerDiv2.className = "col"; headerDiv2.textContent = "General Location"; tableHeader.appendChild(headerDiv2);
+  var headerDiv3 = document.createElement('div'); headerDiv3.className = "col"; headerDiv3.textContent = "Host"; tableHeader.appendChild(headerDiv3);
   parentElem.appendChild(tableHeader);
 }
 
@@ -326,14 +343,13 @@ function CreateOverviewTableRow(array, day, parentElem) {
       var row = document.createElement('li');
       row.className = "table-row clickable-row";
 
-      var col1 = document.createElement('div'); col1.className = "col col-1"; col1.setAttribute('data-label', "Time"); col1.textContent = elem.time; row.appendChild(col1);
-      var col2 = document.createElement('div'); col2.className = "col col-2"; col2.setAttribute('data-label', "General Location"); col2.textContent = elem.location; row.appendChild(col2);
-      var col3 = document.createElement('div'); col3.className = "col col-3"; col3.setAttribute('data-label', "Host"); col3.textContent = elem.host; row.appendChild(col3);
+      var col1 = document.createElement('div'); col1.className = "col"; col1.setAttribute('data-label', "Time"); col1.textContent = elem.time; row.appendChild(col1);
+      var col2 = document.createElement('div'); col2.className = "col"; col2.setAttribute('data-label', "General Location"); col2.textContent = elem.location; row.appendChild(col2);
+      var col3 = document.createElement('div'); col3.className = "col"; col3.setAttribute('data-label', "Host"); col3.textContent = elem.host; row.appendChild(col3);
 
       parentElem.appendChild(row);
     }
   });
-
 }
 
 document.addEventListener('click', function (e) {
@@ -345,63 +361,134 @@ document.addEventListener('click', function (e) {
 //#endregion Overview Setup
 
 //#region Planning Setup
-function PlanningSetup(planRef, isAnonymous) {
+function PlanningSetup(planRef, groupID, isAnonymous) {
   if (isAnonymous) {
-
+    planningBlocked.classList.remove('hide');
+    planningIntro.parentElement.classList.add('hide');
+    planningWeekSelectParent.classList.add('hide');
+    planCurrWeek.replaceChildren();
+    planNextWeek.replaceChildren();
+    planFutureWeek.replaceChildren();
   } else {
+    planningBlocked.classList.add('hide');
+    planningIntro.parentElement.classList.remove('hide');
+    planningWeekSelectParent.classList.remove('hide');
+
     onValue(planRef, (snapshot) => {
       //Show loading indicator
-      if(loader.classList.contains('hide')) {
+      if (loader.classList.contains('hide')) {
         loader.classList.remove('hide');
       }
 
-      snapshot.forEach((childSnapshot) => {
-        const groupKey = childSnapshot.key;
-        const address = childSnapshot.child("Address").val();
-        const email = childSnapshot.child("Email").val();
-        const phone = childSnapshot.child("Phone").val();
+      //Clear current data in array to avoid dups
+      groupInfoArr = [];
 
-        //Update each array with the new data
-        currWeekArr.forEach((elem) => {
-          if (elem.group === groupKey) {
-            elem.address = address;
-            elem.email =  email;
-            elem.phone = phone;
-          }
-        });
-        nextWeekArr.forEach((elem) => {
-          if (elem.group === groupKey) {
-            elem.address = address;
-            elem.email =  email;
-            elem.phone = phone;
-          }
-        });
-        futureWeekArr.forEach((elem) => {
-          if (elem.group === groupKey) {
-            elem.address = address;
-            elem.email =  email;
-            elem.phone = phone;
-          }
+      var childSnapshot = snapshot.child(groupID);
+      const groupKey = childSnapshot.key;
+      const address = childSnapshot.child("Address").val();
+      const description = childSnapshot.child("Description").val();
+      const email = childSnapshot.child("Email").val();
+      var image = childSnapshot.child("Image").val();
+      const phone = childSnapshot.child("Phone").val();
+
+      //Get url for image from storage
+      getDownloadURL(ref_st(storage, "Groups/" + image))
+        .then((url) => {
+          image = url;
+        })
+        .catch((error) => {
+          console.log(error.code + error.message);
         });
 
-        //Clear the current elements in the lists
-        planCurrWeek.replaceChildren();
-        planNextWeek.replaceChildren();
-        planFutureWeek.replaceChildren();
+      //Update group array with the new data
+      const data = { group: groupKey, address: address, description: description, email: email, image: image, phone: phone };
+      groupInfoArr.unshift(data);
 
+      //Clear the current elements in the lists
+      planCurrWeek.replaceChildren();
+      planNextWeek.replaceChildren();
+      planFutureWeek.replaceChildren();
 
+      //Create elements for the lists from each array
+      CreatePlanningTableHeader(planCurrWeek);
+      currWeekArr.forEach((elem) => {
+        if (elem.group === groupKey) {       
+          CreatePlanningTableRow(elem, groupInfoArr, elem.group, planCurrWeek);
+        }
       });
+      CreatePlanningTableHeader(planNextWeek);
+      nextWeekArr.forEach((elem) => {
+        if (elem.group === groupKey) {   
+          CreatePlanningTableRow(elem, groupInfoArr, elem.group, planNextWeek);
+        }
+      });
+      CreatePlanningTableHeader(planFutureWeek);
+      futureWeekArr.forEach((elem) => {
+        if (elem.group === groupKey) {        
+          CreatePlanningTableRow(elem, groupInfoArr, elem.group, planFutureWeek);
+        }
+      });
+
+      //Now Update planning intro to first host and week
+      UpdatePlanningIntro(currWeekArr, groupInfoArr, groupID, planningIntro);
+
+      //Remove loading indicator
+      if (!loader.classList.contains('hide')) {
+        loader.classList.add('hide');
+      }
     });
   }
 }
 
-function CreatePlanningTableHeader(day, parentElem) {
-  var wrapper = document.createElement('li');
-  wrapper.classList.add('table-header');
-  //TODOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+function UpdatePlanningIntro(weekArr, groupInfoArr, groupID, parentElem) {
+  //Show loading indicator
+  if (loader.classList.contains('hide')) {
+    loader.classList.remove('hide');
+  }
+
+  parentElem.children[0].textContent = weekArr.find(w => w.group === groupID).host;
+  parentElem.children[1].children[0].textContent = groupInfoArr.find(g => g.group === groupID).description;
+  parentElem.children[1].children[1].setAttribute("data-groupID", groupID);
+
+  planningImg.setAttribute('src', 'https://firebasestorage.googleapis.com/v0/b/cana-cam.appspot.com/o/Groups%2Fgroup1.jpg?alt=media&token=437b9dea-5684-46db-b31d-bbbd39a24a3e');
+
+  //Remove loading indicator
+  if (!loader.classList.contains('hide')) {
+    loader.classList.add('hide');
+  }
 }
-function CreatePlanningTableRow(array, day, parentElem) {
-  
+
+function CreatePlanningTableHeader(parentElem) {
+  var tableHeader = document.createElement('li');
+  tableHeader.classList.add('table-header');
+  var headerDiv1 = document.createElement('div'); headerDiv1.className = "col"; headerDiv1.textContent = "Day/Time"; tableHeader.appendChild(headerDiv1);
+  var headerDiv2 = document.createElement('div'); headerDiv2.className = "col"; headerDiv2.textContent = "Address"; tableHeader.appendChild(headerDiv2);
+  var headerDiv3 = document.createElement('div'); headerDiv3.className = "col"; headerDiv3.textContent = "Status"; tableHeader.appendChild(headerDiv3);
+  var headerDiv4 = document.createElement('div'); headerDiv4.className = "col"; headerDiv4.textContent = "Guests"; tableHeader.appendChild(headerDiv4);
+  var headerDiv5 = document.createElement('div'); headerDiv5.className = "col"; headerDiv5.textContent = "Action"; tableHeader.appendChild(headerDiv5);
+  parentElem.appendChild(tableHeader);
+}
+
+function CreatePlanningTableRow(elem, groupInfoArr, groupID, parentElem) {
+  var row = document.createElement('li');
+  row.className = "table-row";
+
+  var col1 = document.createElement('div'); col1.className = "col"; col1.setAttribute('data-label', "Day/Time"); col1.textContent = elem.day + "/" + elem.time; row.appendChild(col1);
+  var col2 = document.createElement('div'); col2.className = "col"; col2.setAttribute('data-label', "Address"); col2.textContent = groupInfoArr.find(g => g.group === groupID).address; row.appendChild(col2);
+  var col3 = document.createElement('div'); col3.className = "col"; col3.setAttribute('data-label', "Status"); col3.textContent = "Not Going"; row.appendChild(col3);
+
+  var col4 = document.createElement('div'); col4.className = "col"; col4.setAttribute('data-label', "Guests");
+  var counter = document.createElement('div'); counter.className = "counter";
+  var minus = document.createElement('span'); minus.setAttribute("id", "guest-down"); minus.className = "down"; minus.textContent = "-"; counter.appendChild(minus);
+  var counterInput = document.createElement('input'); counterInput.setAttribute("id", "counter-input"); counterInput.setAttribute("type", "text"); counterInput.value = 0; counterInput.disabled = true; counter.appendChild(counterInput);
+  var plus = document.createElement('span'); plus.setAttribute("id", "guest-up"); plus.className = "up"; plus.textContent = "+"; counter.appendChild(plus);
+  col4.appendChild(counter); row.appendChild(col4);
+
+  var col5 = document.createElement('div'); col5.className = "col"; col5.setAttribute('data-label', "Action");
+  var rsvp = document.createElement('button'); rsvp.className = "rsvp-button";
+  col5.appendChild(rsvp); row.appendChild(col5);
+
+  parentElem.appendChild(row);
 }
 //#endregion Planning Setup
 
