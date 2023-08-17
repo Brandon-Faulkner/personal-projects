72978dcd55c676131c1ddec007766f8110b05422
Brandon-Faulkner <bxf8125@mavs.uta.edu>
2023-08-17 18:44:47 -0500

    Cana Goals: Initial Commit with a basic skeleton page

---
+++ CanaGoals/public/script.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getDatabase, ref, onValue, set, remove, update, get, increment } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+import { getAuth, onAuthStateChanged, reauthenticateWithCredential, signInAnonymously, sendEmailVerification, signInWithEmailAndPassword, updateEmail, sendPasswordResetEmail, EmailAuthProvider, linkWithCredential, signOut } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDYuTQDXbkmGM73f-mBCghdLeTsXLoS9Q",
+  authDomain: "cana-goals.firebaseapp.com",
+  projectId: "cana-goals",
+  storageBucket: "cana-goals.appspot.com",
+  messagingSenderId: "77801609796",
+  appId: "1:77801609796:web:da20da804555c984113a11",
+  measurementId: "G-54LFB530MD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+
+window.addEventListener('load', () => {
+  //Ensure that the browser supports the service worker API then register it
+  var registration = null;
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('service-worker.js').then(reg => {
+      registration = reg;
+      console.log('Service Worker Registered');
+    }).catch(swErr => console.log(`Service Worker Installation Error: ${swErr}}`));
+  }
+});
