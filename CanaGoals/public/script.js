88d725bace2536108236d2a4a26fd1af259df710
Brandon-Faulkner <bxf8125@mavs.uta.edu>
2023-08-19 16:29:25 -0500

    Cana Goals: Added some more styles. Starting to work on connection to DB

--- CanaGoals/public/script.js
+++ CanaGoals/public/script.js
@@ -28,4 +28,58 @@ window.addEventListener('load', () => {
       console.log('Service Worker Registered');
     }).catch(swErr => console.log(`Service Worker Installation Error: ${swErr}}`));
   }
+
+  //Detect users prefered color scheme
+  const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+  document.querySelector("html").setAttribute("data-theme", darkTheme.matches === true ? "dark" : "light");
+
+  //Detect login status and setup tables
+  /*onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // Signed in Anonymously
+      if (user?.isAnonymous) {
+        
+      } else {
+        
+      }
+    } else {
+      // Signed out
+      
+    }
+  });*/
+
+  const tableContainer = document.getElementById("tables-container");
+  tableContainer.replaceChildren();
+  CreateTable("default-table-1", "Edit Name...", 3, 3);
+  CreateTable("default-table-2", "Edit Name...", 3, 3);
+  CreateTable("default-table-3", "Edit Name...", 3, 3);
+
+  function CreateTable(tableID, userName, col, row) {
+    var tableWrap = document.createElement("div"); tableWrap.setAttribute("id", tableID); tableWrap.className = "table-wrapper";
+
+    var h2 = document.createElement("h2"); h2.textContent = userName; h2.contentEditable = true; tableWrap.appendChild(h2);
+
+    var table = document.createElement("table"); table.className = "table-items"; 
+    var thead = document.createElement("thead");
+    var headTr = document.createElement("tr");   
+    for (let i = 0; i < col; i++) {
+      var th = document.createElement("th"); th.textContent = "Header " + (i + 1); 
+      th.contentEditable = "true"; headTr.appendChild(th); 
+    }
+    thead.appendChild(headTr); table.appendChild(thead);
+
+    var tbody = document.createElement("tbody");
+    for (let i = 0; i < col; i++) {
+      var bodyTr = document.createElement("tr");
+      for (let j = 0; j < row; j++) {
+        var td = document.createElement("td"); td.textContent = "Content " + (i + 1); 
+        td.contentEditable = "true"; bodyTr.appendChild(td);
+      }
+      tbody.appendChild(bodyTr);
+    }
+    table.appendChild(tbody);
+
+    tableWrap.appendChild(table);
+    tableContainer.appendChild(tableWrap);
+  }
 });
