76ae3d1db8b16cf79593f68b3d53ced7c72829a0
Brandon-Faulkner <bxf8125@mavs.uta.edu>
2023-12-21 19:37:32 -0600

    LaunchPad: Completed overhaul of leaderboard. Might add error notifs.

--- LaunchPad/public/leaderboard/script.js
+++ LaunchPad/public/leaderboard/script.js
@@ -1,26 +1,50 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
-import { getDatabase, ref, onValue, child, query, orderByChild, limitToLast, onChildAdded } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
+import { getDatabase, ref, onValue, child, update } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCkhBqKXXHQcgk9QYS7TTCY9I1kjx_bowk",
-  authDomain: "cana-launchpad.firebaseapp.com",
-  projectId: "cana-launchpad",
-  storageBucket: "cana-launchpad.appspot.com",
-  messagingSenderId: "47186518351",
-  appId: "1:47186518351:web:d40c2d357ead5636b8653a"
+    apiKey: "AIzaSyCkhBqKXXHQcgk9QYS7TTCY9I1kjx_bowk",
+    authDomain: "cana-launchpad.firebaseapp.com",
+    projectId: "cana-launchpad",
+    storageBucket: "cana-launchpad.appspot.com",
+    messagingSenderId: "47186518351",
+    appId: "1:47186518351:web:d40c2d357ead5636b8653a"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Realtime Database
 const database = getDatabase(app);
+const userAppRef = ref(database, 'UserApp/');
+
+function FadeInOut(elem, show) {
+    if (show == true) {
+        elem.classList.remove("fadeOut");
+        elem.style = "";
+        elem.classList.add("fadeIn");
+    } else {
+        elem.classList.remove("fadeIn");
+        elem.classList.add("fadeOut");
+        setTimeout(() => {
+            elem.style = "display: none";
+        }, 200);
+    }
+}
+
+function ClearLeaderboard(boardNames) {
+    boardNames.forEach((board) => {
+        for (let i = 0; i < 5; i++) {
+            document.getElementById(board + i).textContent = null;
+        }
+    });
+}
 
 window.addEventListener('load', function () {
-    //Connect to the UserApp path in db to access values
-    const userAppRef = ref(database, 'UserApp/');
+    //Firebase listener unsubscribers
+    var donorUnsub = null;
+    var prayerUnsub = null;
+    var teamUnsub = null;
+
     var currentProject = null;
     var currentSponsor = null;
     var isDonorBoardOn = true;
@@ -30,161 +54,112 @@ window.addEventListener('load', function () {
     const projectOwnerName = document.getElementById('project-owner-name');
     const sponsorName = document.getElementById('sponsor-name');
     const donorBoard = document.getElementById('donor');
+    const donorTotal = document.getElementById('donor-total');
     const prayerBoard = document.getElementById('prayer');
+    const prayerTotal = document.getElementById('prayer-total');
     const teamBoard = document.getElementById('team');
+    const teamTotal = document.getElementById('team-total');
+
+    const currencyFormatter = new Intl.NumberFormat(undefined, {
+        style: 'currency',
+        currency: 'USD', minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+    });
 
     onValue(userAppRef, (snapshot) => {
 
         //Get and set all the values from db
-        snapshot.forEach((childSnapshot) => {
-            switch (childSnapshot.key) {
-                case "CurrentProject":
-                    currentProject = childSnapshot.val();
-                    break;
-                case "CurrentSponsor":
-                    currentSponsor = childSnapshot.val();
-                    break;
-                case "isDonorBoardOn":
-                    isDonorBoardOn = childSnapshot.val();
-                    break;
-                case "isPrayerBoardOn":
-                    isPrayerBoardOn = childSnapshot.val();
-                    break;
-                case "isTeamBoardOn":
-                    isTeamBoardOn = childSnapshot.val();
-                    break;
-            }
-        });
+        if (snapshot.exists()) {
+            currentProject = snapshot.child("CurrentProject").val();
+            currentSponsor = snapshot.child("CurrentSponsor").val();
+            isDonorBoardOn = snapshot.child("isDonorBoardOn").val();
+            isPrayerBoardOn = snapshot.child("isPrayerBoardOn").val();
+            isTeamBoardOn = snapshot.child("isTeamBoardOn").val();
+        }
 
         //Upate the leaderboard with owner, sponsor and board values
-        projectOwnerName.innerText = currentProject;
-        sponsorName.innerText = currentSponsor;
+        projectOwnerName.textContent = currentProject;
+        sponsorName.textContent = currentSponsor;
 
-        if (isDonorBoardOn === false) {
-            if (!donorBoard.classList.contains('fade')) {
-                donorBoard.classList.toggle('fade');
-            }
-        }
-        else {
-            if (donorBoard.classList.contains('fade')) {
-                donorBoard.classList.toggle('fade');
-            }
-        }
-
-        if (isPrayerBoardOn === false) {
-            if (!prayerBoard.classList.contains('fade')) {
-                prayerBoard.classList.toggle('fade');
-            }
-        }
-        else {
-            if (prayerBoard.classList.contains('fade')) {
-                prayerBoard.classList.toggle('fade');
-            }
-        }
-
-        if (isTeamBoardOn === false) {
-            if (!teamBoard.classList.contains('fade')) {
-                teamBoard.classList.toggle('fade');
-            }
-        }
-        else {
-            if (teamBoard.classList.contains('fade')) {
-                teamBoard.classList.toggle('fade');
-            }
-        }
+        //Fade in/out the boards and corresponding total
+        FadeInOut(donorBoard, isDonorBoardOn); FadeInOut(donorTotal, isDonorBoardOn);
+        FadeInOut(prayerBoard, isPrayerBoardOn); FadeInOut(prayerTotal, isPrayerBoardOn);
+        FadeInOut(teamBoard, isTeamBoardOn); FadeInOut(teamTotal, isTeamBoardOn);
 
         try {
-            //Get the names and pledges from database to add to leaderboard
-            const donorNames = query(ref(database, 'Projects/' + currentProject + "/Board:Donors/Names/"), orderByChild('/Pledge'), limitToLast(5));
-            const prayerNames = query(ref(database, 'Projects/' + currentProject + "/Board:PrayerPartners/Names/"), limitToLast(5));
-            const teamNames = query(ref(database, 'Projects/' + currentProject + "/Board:TeamMembers/Names/"), limitToLast(5));
-            const pledgeTotalRef = ref(database, 'Projects/' + currentProject + "/Board:Donors/Names/", orderByChild('Pledge'));
-            var currencyFormatter = new Intl.NumberFormat('en-US', {style:'currency', currency:'USD', maximumFractionDigits:0});
+            //Clear the leaderboard initially before adding new names
+            ClearLeaderboard(["donor", "prayer", "team"]);
 
-            async function clearLeaderboard(board) {
-                //Clear each leaderboard spot before adding new names
-                for (let i = 0; i < 5; i++) {
-                    if (board === "donor") {
-                        document.getElementById("donor" + i).innerText = null;
-                    }
-                    else if (board === "prayer") {
-                        document.getElementById("prayer" + i).innerText = null;
-                    }
-                    else if (board === "team") {
-                        document.getElementById("team" + i).innerText = null;
-                    }
-                    else {
-                        document.getElementById("donor" + i).innerText = null;
-                        document.getElementById("prayer" + i).innerText = null;
-                        document.getElementById("team" + i).innerText = null;
-                    }
-                }
+            //Listeners to get each board's data
+            if (isDonorBoardOn) {
+                donorUnsub == null ? null : donorUnsub();
+                donorUnsub = onValue(ref(database, 'Projects/' + currentProject + "/Board:Donors/Names/"), (snapshot) => {
+                    const donorArray = [];
+                    var totalPledges = 0;
+                    snapshot.forEach((data) => {
+                        var pledgeAmount = data.child("Pledge").val();
+                        donorArray.push({ name: data.child("Name").val(), pledge: pledgeAmount });
+                        totalPledges += pledgeAmount;
+                    });
+                    donorArray.sort((a, b) => (b.pledge - a.pledge));
 
-                return 1;
+                    ClearLeaderboard(["donor"]);
+                    for (let i = 0; i < donorArray.slice(0,5).length; i++) {
+                        document.getElementById("donor" + i).textContent =
+                            donorArray[i].name + " " + currencyFormatter.format(donorArray[i].pledge);
+                    }
+
+                    donorTotal.textContent = "Pledges: " + currencyFormatter.format(totalPledges);
+                    const updates = {}; updates['Projects/' + currentProject + '/Totals/Pledges'] = currencyFormatter.format(totalPledges);
+                    update(ref(database), updates);
+                });
+            } else {
+                donorUnsub == null ? null : donorUnsub();
             }
 
-            //Clear the leaderboard initially before adding new names
-            clearLeaderboard("all");
-
-            //Take the snapshot into an array and reverse it to display properly
-            onValue(donorNames, (snapshot) => {
-                const snapShotArray = [];
-                snapshot.forEach((data) => {
-                    snapShotArray.push(data.child("Name").val() + " " + currencyFormatter.format(data.child("Pledge").val()));
+            if (isPrayerBoardOn) {
+                prayerUnsub == null ? null : prayerUnsub();
+                prayerUnsub = onValue(ref(database, 'Projects/' + currentProject + "/Board:PrayerPartners/Names/"), (snapshot) => {
+                    const prayerArray = [];
+                    snapshot.forEach((data) => {
+                        prayerArray.unshift(data.val());
+                    });
+    
+                    ClearLeaderboard(["prayer"]);
+                    for (let i = 0; i < prayerArray.slice(0,5).length; i++) {
+                        document.getElementById("prayer" + i).textContent = prayerArray[i];
+                    }
+    
+                    prayerTotal.textContent = "Partners: " + snapshot.size;
+                    const updates = {}; updates['Projects/' + currentProject + '/Totals/Partners'] = snapshot.size;
+                    update(ref(database), updates);
                 });
-
-                snapShotArray.reverse();
-
-                clearLeaderboard("donor");
-
-                for (let i = 0; i < snapShotArray.length; i++) {
-                    document.getElementById("donor" + i).innerText = snapShotArray[i];
-                }
-            });
-
-            onValue(pledgeTotalRef, (snapshot) => {
-                var pledgeTotal = 0;
-                snapshot.forEach((data) => {
-                    pledgeTotal += data.child("Pledge").val();
+            } else {
+                prayerUnsub == null ? null : prayerUnsub();
+            }
+            
+            if (isTeamBoardOn) {
+                teamUnsub == null ? null : teamUnsub();
+                teamUnsub = onValue(ref(database, 'Projects/' + currentProject + "/Board:TeamMembers/Names/"), (snapshot) => {
+                    const teamArray = [];
+                    snapshot.forEach((data) => {
+                        teamArray.unshift(data.val());
+                    });
+    
+                    ClearLeaderboard(["team"]);
+                    for (let i = 0; i < teamArray.slice(0,5).length; i++) {
+                        document.getElementById("team" + i).textContent = teamArray[i];
+                    }
+    
+                    teamTotal.textContent = "Members: " + snapshot.size;
+                    const updates = {}; updates['Projects/' + currentProject + '/Totals/Members'] = snapshot.size;
+                    update(ref(database), updates);
                 });
-
-                document.getElementById('donor-title').innerText = "Donors & Pledges / Total: " + currencyFormatter.format(pledgeTotal);
-            });
-
-            //Take the snapshot into an array and reverse it to display properly
-            onValue(prayerNames, (snapshot) => {
-                const snapShotArray = [];
-                snapshot.forEach((data) => {
-                    snapShotArray.push(data.val());
-                });
-
-                snapShotArray.reverse();
-
-                clearLeaderboard("prayer");
-
-                for (let i = 0; i < snapShotArray.length; i++) {
-                    document.getElementById("prayer" + i).innerText = snapShotArray[i];
-                }
-            });
-
-            //Take the snapshot into an array and reverse it to display properly
-            onValue(teamNames, (snapshot) => {
-                const snapShotArray = [];
-                snapshot.forEach((data) => {
-                    snapShotArray.push(data.val());
-                });
-
-                snapShotArray.reverse();
-
-                clearLeaderboard("team");
-
-                for (let i = 0; i < snapShotArray.length; i++) {
-                    document.getElementById("team" + i).innerText = snapShotArray[i];
-                }
-            });
+            } else {
+                teamUnsub == null ? null : teamUnsub();
+            }    
         } catch (error) {
             console.log(error);
-            //window.location.reload();
         }
     });
 });
